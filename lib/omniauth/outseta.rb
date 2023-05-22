6d261ca971aba2ed6df796d6dea5f25a171b2922
Matt Webb <hello@mattwebb.io>
2023-05-22 08:56:49 -0700

    chore: generate `omniauth-outseta` gem

---
+++ lib/omniauth/outseta.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "outseta/version"
+
+module Omniauth
+  module Outseta
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
